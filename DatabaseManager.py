974cb5d6ff5d22f21aab73471c93a43c1af330c3
Maurício <mauricioarruda660@gmail.com>
2023-09-14 21:45:49 -0300

    Azure Database
    Utilização de banco de dados Azure

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
-senha = quote_plus('P@ssw0rd!')
-engine = create_engine(f'mssql+pyodbc://sa:{senha}@localhost/Dados?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes')
+senha = quote_plus('P@ssw0rd')
+servidor = 'nameserver-prd.mysql.database.azure.com'
+banco_de_dados = 'NameServer'
+usuario = 'sadmin01'
+porta = 3306
+
+# Crie o URL de conexão para o MySQL na Azure
+engine = create_engine(f"mysql+mysqlconnector://{usuario}:{senha}@{servidor}:{porta}/{banco_de_dados}")
 
 _SessionFactory = sessionmaker(bind=engine)
 
